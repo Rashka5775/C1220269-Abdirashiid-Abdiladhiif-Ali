148f512c16a1dea993c33e3e6ed599dc7d7e5533
RAQIIS COMPUTERS <cabdirashiidcabdiladhiif338@gmail.com>
2025-05-01 09:55:44 -0800

    first commit

---
+++ Assigments/Assigments/src/test/java/demo/Assigments/AssigmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.Assigments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssigmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
